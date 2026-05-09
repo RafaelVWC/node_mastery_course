10b2ce7e2f177e35231b35e6ca750fd2a3053d93
Rafael <rafaelvianna2001@gmail.com>
2026-05-09 00:20:51 -0300

    Express

---
+++ 6_EXPRESS/11_tarefa_03/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+const port = 5000
+
+const path = require("path")
+
+const users = require("./users")
+
+//read body
+
+app.use(
+    express.urlencoded({
+        extended:true,
+    }),
+)
+
+app.use(express.json())
+
+//static files
+
+app.use(express.static('public'))
+
+
+const basePath = path.join(__dirname, 'templates')
+app.use('/users',users)
+
+app.get('/', (req,res) =>{
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.use(function(req,res,next){
+
+    res.status(404).sendFile(`${basePath}/404.html`)
+    
+})
+
+
+app.listen(port, ()=>{
+    console.log(`App rodando na porta ${port}`)
+})
